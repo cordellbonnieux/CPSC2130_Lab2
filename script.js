ec21a5ccd326e4fef5037eea81de929af9145ae2
Cordell <cordellbonnieux@gmail.com>
2022-01-27 19:11:15 -0800

    Added a random banana spawner, which floads the canvas with bananas (the theme).

--- script.js
+++ script.js
@@ -18,17 +18,36 @@ function addMonkey() {
     }
 }
 
-function drawBanana() {
+function drawBanana(x, y) {
     ctx.beginPath();
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'yellow';
     ctx.lineWidth = 5;
-    ctx.moveTo(400,300);
-    ctx.bezierCurveTo(450, 300, 510, 220, 480, 200);
-    ctx.lineTo(490, 195);
-    //ctx.LineTo();
-    //ctx.bezierCurveTo(520, 300, 480, 300, 450, 300);
+    ctx.moveTo(x, y); // 400, 300
+    ctx.bezierCurveTo(
+        //450, 300, 510, 220, 480, 200
+        (x+50), y, 
+        (x+110), (y-80), 
+        (x+80), (y-100)
+    );
+    ctx.lineTo(x + 88, y - 110); //488, 190
+    ctx.bezierCurveTo(
+        //550, 300, 450, 310, 400, 300
+        (x+150), y,
+        (x+50),(y+10),
+        x, y
+        );
+    ctx.fill();
     ctx.stroke();
+}
+
+function drawBananas() {
+    const amountOfBananas = Math.random() * (100 - 20) + 20;
+    for (let i = 0; i < amountOfBananas; i++) {
+        const x = Math.random() * ((canvas.width - 75) - 75) + 75;
+        const y = Math.random() * ((canvas.height - 75) - 75) + 75;
+        drawBanana(x, y);
+    }
 }
 
 function drawGround() {
@@ -38,9 +57,10 @@ function drawGround() {
 
 function drawShapes() {
     drawGround();
-    drawBanana();
+    drawBananas();
+
     
 }
 
 drawShapes();
-//addMonkey();
+addMonkey();
