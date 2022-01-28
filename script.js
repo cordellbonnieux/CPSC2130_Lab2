d270e7d99e11ab3f02cf32fbd3c0f4617e4a6fae
Cordell <cordellbonnieux@gmail.com>
2022-01-27 20:24:38 -0800

    Made a chat bubble for monkey.

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ canvas.width = 768;
 canvas.height = 576;
 const ctx = canvas.getContext('2d');
 
+draw();
+
+/* Draws all images the the screen */
+function draw() {
+    drawGround();
+    drawBananas();
+    addMonkey();
+    drawBubble();
+}
+
 /* Draw a monkey image to canvas and crop */
 function addMonkey() {
     const img = new Image();
@@ -20,6 +30,7 @@ function addMonkey() {
 
 /* Spawns a banana at (x, y) */
 function drawBanana(x, y) {
+    /* Requirement: A shape created  */
     ctx.beginPath();
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'yellow';
@@ -36,6 +47,7 @@ function drawBanana(x, y) {
         (x+50),(y+10),
         x, y
         );
+    ctx.closePath();
     ctx.fill();
     ctx.stroke();
 }
@@ -44,23 +56,42 @@ function drawBanana(x, y) {
 function drawBananas() {
     const amountOfBananas = Math.random() * (100 - 20) + 20;
     for (let i = 0; i < amountOfBananas; i++) {
-        const x = Math.random() * ((canvas.width - 75) - 75) + 75;
-        const y = Math.random() * ((canvas.height - 75) - 75) + 75;
+        const x = Math.random() * ((canvas.width - 75) - 10) + 10;
+        const y = Math.random() * ((canvas.height - 75) - 10) + 10;
         drawBanana(x, y);
     }
 }
 
 /* Draws the ground Rect */
 function drawGround() {
+    /* Requirement: Rectangle */
     ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 10;
+    ctx.strokeRect(0, (canvas.height / 3) * 2, canvas.width, canvas.height / 3);
     ctx.fillRect(0, (canvas.height / 3) * 2, canvas.width, canvas.height / 3);
+    ctx.stroke();
 }
 
-/* Draws all images the the screen */
-function draw() {
-    drawGround();
-    drawBananas();
-    addMonkey();
+/* Draws the comic-style text buddle */
+function drawBubble(text) {
+    text = "I love bananas";
+    ctx.fillStyle = '#fff';
+    // make the backing for the main bubble
+    ctx.beginPath();
+    ctx.arc(525, 400, 152, 0, 2 * Math.PI);
+    ctx.stroke();
+    // make the triangle part
+    ctx.beginPath()
+    ctx.moveTo(300, 325);
+    ctx.lineTo(500, 300);
+    ctx.lineTo(425, 475);
+    ctx.lineTo(300, 325);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    // make the cirle part
+    ctx.beginPath();
+    ctx.arc(525, 400, 150, 0, 2 * Math.PI);
+    ctx.fill();
 }
-
-draw();
