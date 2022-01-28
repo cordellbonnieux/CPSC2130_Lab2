6a82ea2b5572206a79ef7870f520ca47d211f1c2
Cordell <cordellbonnieux@gmail.com>
2022-01-27 19:15:16 -0800

    Cleaned, and commented program.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ canvas.width = 768;
 canvas.height = 576;
 const ctx = canvas.getContext('2d');
 
-/* Draw image to canvas */
+/* Draw a monkey image to canvas and crop */
 function addMonkey() {
     const img = new Image();
     img.src = 'img/monkey.jpg';
@@ -18,21 +18,20 @@ function addMonkey() {
     }
 }
 
+/* Spawns a banana at (x, y) */
 function drawBanana(x, y) {
     ctx.beginPath();
     ctx.strokeStyle = 'black';
     ctx.fillStyle = 'yellow';
     ctx.lineWidth = 5;
-    ctx.moveTo(x, y); // 400, 300
+    ctx.moveTo(x, y);
     ctx.bezierCurveTo(
-        //450, 300, 510, 220, 480, 200
         (x+50), y, 
         (x+110), (y-80), 
         (x+80), (y-100)
     );
-    ctx.lineTo(x + 88, y - 110); //488, 190
+    ctx.lineTo(x + 88, y - 110);
     ctx.bezierCurveTo(
-        //550, 300, 450, 310, 400, 300
         (x+150), y,
         (x+50),(y+10),
         x, y
@@ -41,6 +40,7 @@ function drawBanana(x, y) {
     ctx.stroke();
 }
 
+/* Spawns a lot of bananas all over the place (it's bananas) */
 function drawBananas() {
     const amountOfBananas = Math.random() * (100 - 20) + 20;
     for (let i = 0; i < amountOfBananas; i++) {
@@ -50,17 +50,17 @@ function drawBananas() {
     }
 }
 
+/* Draws the ground Rect */
 function drawGround() {
     ctx.fillStyle = 'green';
     ctx.fillRect(0, (canvas.height / 3) * 2, canvas.width, canvas.height / 3);
 }
 
-function drawShapes() {
+/* Draws all images the the screen */
+function draw() {
     drawGround();
     drawBananas();
-
-    
+    addMonkey();
 }
 
-drawShapes();
-addMonkey();
+draw();
